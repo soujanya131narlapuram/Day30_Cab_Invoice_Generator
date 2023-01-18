9334bfbae7c1d005ca2e018b50fb18ca0b6d8d95
Soujanya <soujanya131narlapuram@gmail.com>
2023-01-18 19:47:14 +0530

    'UC2 Commit'

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CabInvoiceGenerator {
@@ -9,18 +10,47 @@ public class CabInvoiceGenerator {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter distance in kilometers: ");
-        double distance = input.nextDouble();
-        System.out.print("Enter time in minutes: ");
-        double time = input.nextDouble();
-        double fare = calculateFare(distance, time);
-        System.out.println("Total fare for the journey is: " + fare);
+        ArrayList<Ride> rides = new ArrayList<>();
+        while (true) {
+            System.out.println("1. Add Ride");
+            System.out.println("2. Generate Invoice");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = input.nextInt();
+            input.nextLine();
+            if (choice == 1) {
+                System.out.print("Enter distance in kilometers: ");
+                double distance = input.nextDouble();
+                System.out.print("Enter time in minutes: ");
+                double time = input.nextDouble();
+                rides.add(new Ride(distance, time));
+            } else if (choice == 2) {
+                double fare = calculateFare(rides);
+                System.out.println("Total fare for the rides is: " + fare);
+            } else if (choice == 3) {
+                break;
+            }
+        }
         input.close();
     }
 
-    public static double calculateFare(double distance, double time) {
-        double fare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
-        return fare < MINIMUM_FARE ? MINIMUM_FARE : fare;
+    public static double calculateFare(ArrayList<Ride> rides) {
+        double fare = 0;
+        for (Ride ride : rides) {
+            fare += ride.distance * COST_PER_KILOMETER + ride.
+                    time * COST_PER_MINUTE;
+        }
+        return fare < MINIMUM_FARE * rides.size() ? MINIMUM_FARE * rides.size() : fare;
+    }
+
+    static class Ride {
+        public double distance;
+        public double time;
+
+        public Ride(double distance, double time) {
+            this.distance = distance;
+            this.time = time;
+        }
     }
 }
 
