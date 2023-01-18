2b75a65180c8d64cf15edf750a7b59f2b1de7a95
Soujanya <soujanya131narlapuram@gmail.com>
2023-01-18 20:05:04 +0530

    'UC4 Commit'

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,30 +1,39 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class CabInvoiceGenerator {
     public static final double COST_PER_KILOMETER = 10;
     public static final double COST_PER_MINUTE = 1;
     public static final double MINIMUM_FARE = 5;
+    public static HashMap<String, ArrayList<Ride>> rideRepository = new HashMap<>();
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<Ride> rides = new ArrayList<>();
         while (true) {
-            System.out.println("1. Add Ride");
+            System.out.println("1. Add a ride for user");
             System.out.println("2. Generate Invoice");
             System.out.println("3. Exit");
             System.out.print("Enter your choice: ");
             int choice = input.nextInt();
             input.nextLine();
             if (choice == 1) {
+                System.out.print("Enter user id: ");
+                String userId = input.nextLine();
                 System.out.print("Enter distance in kilometers: ");
                 double distance = input.nextDouble();
                 System.out.print("Enter time in minutes: ");
                 double time = input.nextDouble();
-                rides.add(new Ride(distance, time));
+                addRide(userId, distance, time);
             } else if (choice == 2) {
+                System.out.print("Enter user id: ");
+                String userId = input.nextLine();
+                ArrayList<Ride> rides =new ArrayList<>();
+                if (rideRepository.containsKey(userId)){
+                    rides = rideRepository.get(userId);
+                }
                 double fare = calculateFare(rides);
                 System.out.println("Total number of rides: " + rides.size());
                 System.out.println("Total fare for the rides: " + fare);
@@ -34,6 +43,17 @@ public class CabInvoiceGenerator {
             }
         }
         input.close();
+    }
+
+    public static void addRide(String userId, double distance, double time){
+        if(rideRepository.containsKey(userId)){
+            rideRepository.get(userId).add(new Ride(distance,time));
+        }
+        else{
+            ArrayList<Ride> rides = new ArrayList<>();
+            rides.add(new Ride(distance,time));
+            rideRepository.put(userId,rides);
+        }
     }
 
     public static double calculateFare(ArrayList<Ride> rides) {
